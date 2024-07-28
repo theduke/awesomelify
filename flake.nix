154a487b122a70e9c9a5add947e7294faf3efaab
Christoph Herzog <chris@theduke.at>
2024-07-28 16:04:30 +0200

    build: Add nix flake

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "awesomelify";
+
+  inputs = {
+    flakeutils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flakeutils }:
+    flakeutils.lib.eachDefaultSystem (system:
+      let
+        NAME = "awesomelify";
+        VERSION = "0.1";
+
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        lib = pkgs.lib;
+        hasPrefix = lib.hasPrefix;
+      in
+      rec {
+
+
+        packages.${NAME} = pkgs.rustPlatform.buildRustPackage rec {
+          pname = NAME;
+          version = VERSION;
+
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./Cargo.toml
+              ./Cargo.lock
+              ./src
+            ];
+          };
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = [
+          ];
+
+          buildInputs = [
+          ];
+
+          checkPhase = ''
+            echo tests...
+          '';
+
+          meta = {
+            description = "awesomelify server";
+            homepage = "https://github.com/theduke/awesomelify";
+            license = pkgs.lib.licenses.mit;
+            maintainers = [];
+          };
+        };
+
+        defaultPackage = packages.${NAME};
+
+        # For `nix run`.
+        apps.${NAME} = flakeutils.lib.mkApp {
+          drv = packages.${NAME};
+        };
+        defaultApp = apps.${NAME};
+
+        devShell = pkgs.mkShell {
+          env.RUST_LOG = "awesomelify=trace,reels_db=trace,info";
+
+          packages = with pkgs; [
+            just
+            cargo-nextest
+          ];
+        };
+      }
+    );
+}
